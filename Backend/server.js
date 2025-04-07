c3bc20878ed27fbdc8470fe0bb92480d53976a81
sujoyDey1202 <166387261+sujoyDey1202@users.noreply.github.com>
2025-04-07 19:24:55 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const app = require("./src/app");
+const PORT = process.env.PORT||3000;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on  http://localhost:3000`);
 })
